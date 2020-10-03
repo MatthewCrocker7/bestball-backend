213cd47f372bb7350536268e837aac85ec23c443
Matthew Crocker <matthewcroc@gmail.com>
2020-10-02 23:05:52 -0500

    Set up scheduler for player rounds and team rounds. Completed game enrichment

--- src/main/java/crocker/golf/bestball/core/scheduler/GameUpdateScheduler.java
+++ src/main/java/crocker/golf/bestball/core/scheduler/GameUpdateScheduler.java
@@ -16,7 +16,7 @@ public class GameUpdateScheduler {
         this.gameManagerService = gameManagerService;
     }
 
-    @Scheduled(fixedDelayString = "${golf.pga.update.rate.round}", initialDelay = 2000)
+    @Scheduled(fixedDelayString = "${golf.pga.update.rate.round}", initialDelay = 60000)
     @Async
     public void updateTeamScores() {
         logger.info("Updating team scores on thread {}", Thread.currentThread().getName());
